a2e0ffb49e761b52c4d71c226552018c068ae963
Tatyana0303 <tatyana.kolesnik1992@gmail.com>
2021-08-03 18:44:29 +0300

    first commit

---
+++ 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1
+//
+//  Created by Tania on 03.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
